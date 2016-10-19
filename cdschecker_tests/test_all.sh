5f3e6a0b733c7f96e89091d07e0fb40206ef0d27
Paul Thomson <paul.thomson11@imperial.ac.uk>
2016-10-19 17:19:58 +0100

    Updated cdschecker test_all script to skip one benchmark and not execute make.

--- cdschecker_tests/test_all.sh
+++ cdschecker_tests/test_all.sh
@@ -2,9 +2,9 @@
 set -e
 set -u
 
-make
+# skip spsc-queue as it seems to deadlock.
 
-for t in barrier chase-lev-deque dekker-fences linuxrwlocks mcs-lock mpmc-queue ms-queue spsc-queue ; do
+for t in barrier chase-lev-deque dekker-fences linuxrwlocks mcs-lock mpmc-queue ms-queue; do
   cd $t
   echo -n "$t " 
   ../test.sh ./$t
