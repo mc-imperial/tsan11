7f99e63a001002d4dd61b98f73c40a25820d5b55
Paul Thomson <paul.thomson11@imperial.ac.uk>
2016-10-23 11:26:56 +0100

    Small fix.

--- scripts/tsan11.py
+++ scripts/tsan11.py
@@ -73,7 +73,7 @@ def copytree(src, dst, symlinks=False, ignore=None):
     for item in os.listdir(src):
         s = os.path.join(src, item)
         d = os.path.join(dst, item)
-        if os.path.isdir(s) and ((not os.path.islink()) or symlinks):
+        if os.path.isdir(s) and ((not os.path.islink(s)) or symlinks):
             copytree(s, d, symlinks, ignore)
         else:
             if not os.path.exists(d) or os.stat(s).st_mtime - os.stat(d).st_mtime > 1:
