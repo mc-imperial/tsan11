a5ac44c542d47fac896f682f7219af0d5ba8608d
Paul Thomson <paul.thomson11@imperial.ac.uk>
2016-10-23 10:05:52 +0100

    Try getting LLVM 3.9 instead of some revision.

--- scripts/get-llvm.sh
+++ scripts/get-llvm.sh
@@ -3,14 +3,14 @@ set -x
 set -e
 set -u
 
-svn checkout --revision 272792 http://llvm.org/svn/llvm-project/llvm/trunk .
+svn checkout http://llvm.org/svn/llvm-project/llvm/tags/RELEASE_390/final .
 cd tools
-svn checkout --revision 272792 http://llvm.org/svn/llvm-project/cfe/trunk clang
+svn checkout http://llvm.org/svn/llvm-project/cfe/tags/RELEASE_390/final clang
 cd ..
 cd projects
 svn checkout --revision 272792 http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-svn checkout --revision 272792 http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-svn checkout --revision 272792 http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+svn checkout http://llvm.org/svn/llvm-project/libcxx/tags/RELEASE_390/final libcxx
+svn checkout http://llvm.org/svn/llvm-project/libcxxabi/tags/RELEASE_390/final libcxxabi
 cd ..
 
 
