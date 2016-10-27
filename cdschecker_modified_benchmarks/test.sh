85fb94bb24adb85e2dd675648c3f44e0715eee22
Christopher Lidbury <christopher.lidbury10@imperial.ac.uk>
2016-10-27 17:30:24 +0100

    Improve CDSChecker test script.

--- cdschecker_modified_benchmarks/test.sh
+++ cdschecker_modified_benchmarks/test.sh
@@ -21,8 +21,12 @@ for i in `seq 1 1 $TOTAL_RUN` ; do
   COUNT_TIME=$((COUNT_TIME + TIME_EXE))
 done
 
-AVG_DATA_RACE=$((COUNT_DATA_RACE * 100 / TOTAL_RUN))
-AVG_TIME=$((COUNT_TIME / TOTAL_RUN))
+AVG_DATA_RACE=$(echo "${COUNT_DATA_RACE} * 100 / ${TOTAL_RUN}" | bc -l | xargs printf "%.1f")
+AVG_TIME_INT=$(echo "${COUNT_TIME} / ${TOTAL_RUN} + 0.5" | bc -l | xargs printf "%.0f")
 
-echo "Runs: $TOTAL_RUN Data_races: $COUNT_DATA_RACE Time: ${COUNT_TIME} ms Data_race_rate: ${AVG_DATA_RACE} % Avg_time: ${AVG_TIME} ms"
+# -3 / log(1 - p) < n
+NO_99=$(echo "-3 / (l(1 - (${AVG_DATA_RACE} / 100)) / l(10)) + 0.5" | bc -l | xargs printf "%.0f")
+TIME_99=$(echo "${NO_99} * ${AVG_TIME_INT}" | bc -l)
 
+echo "Runs: $TOTAL_RUN | Data races: $COUNT_DATA_RACE | Total time: ${COUNT_TIME}ms"
+echo "Time: ${AVG_TIME_INT}ms | Race rate: ${AVG_DATA_RACE}% | No. 99.9%: ${NO_99} | Time 99.9%: ${TIME_99}ms"
